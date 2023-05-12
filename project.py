f9253a4e314567bdfafd49b5ed41fd2de7d7a702
Aashi <jadhav.aashi@gmail.com>
2023-05-11 21:17:57 -0400

    update progess code hass been added in 'file_organizer' function  to print out progess message every 5 files

--- project.py
+++ project.py
@@ -5,8 +5,6 @@ import csv
 import random
 import re
 from openpyxl import Workbook
-from tqdm import tqdm
-import signal
 
 # Define custom exception for directory already exists
 class DirectoryExistsError(Exception):
@@ -25,48 +23,6 @@ except DirectoryExistsError as e:
     # Print error message if directory already exists
     print("Error:", e)
 
-
-def search_files(directory_path, text_pattern):
-    try:
-        # Define the file extensions to search for
-        file_extensions = (".txt", ".doc", ".csv", ".xlsx", ".docx",)        
-        # Create an empty list to store the matching file paths
-        files_found = []       
-        # Compile the regular expression pattern
-        pattern = re.compile(text_pattern, re.IGNORECASE)
-        # Use os.walk() to recursively search for files in the directory
-        for subdir, dirs, files in os.walk(directory_path):
-            for file in files:
-                # Check if the file extension matches any of the desired extensions
-                file_ext = os.path.splitext(file)[-1].lower()
-                if file_ext in file_extensions:
-                    # If a match is found, add the file path to the list
-                    file_path = os.path.join(subdir, file)
-                    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-                        for line in f:
-                            if pattern.search(line):
-                                files_found.append(file_path)
-                                break
-        # If one or more files are found, return their paths and count
-        if files_found:
-            result = "Files found:\n"
-            for file in files_found:
-                result += f"File name: {os.path.basename(file)}\nFile path: {file}\n"
-            result += f"Total number of search files: {len(files_found)}"
-            print(result)
-            return result
-        else:
-            print("No files found.")
-            return "No files found."  
-    except FileNotFoundError as e:
-        error_msg = f"Error: {e.strerror}: '{directory_path}'"
-        print(error_msg)
-        raise Exception(error_msg)
-    except Exception as e:
-        error_msg = f"Error: {e}"
-        print(error_msg)
-        raise Exception(error_msg)
-    
 def create_inventory_data_text(file_name, directory_path):
     # Creates a list of tuples called inventory_data that contains the name, units in stock, and days' supply for each drug
     inventory_data = [("Acetaminophen", 1000, 30),
@@ -159,58 +115,48 @@ def create_excel(file_name, directory_path):
     workbook.save(os.path.join(directory_path, file_name))
     print('Excel file created: ', file_name)
 
-""" def file_organizer(directory):
-   # Get the list of all files in the directory
-    #breakpoint()  # Pause to inspect the files list
-    files = os.listdir(directory)
-    print("Files in directory:", files)  # Print the files list for inspection
-    # Loop through each file in the directory
-    for file in files:
-        # Get the full file path
-        file_path = os.path.join(directory, file)
-        print("File path:", file_path)  # Print the file path for inspection
-        #breakpoint()  # Pause to inspect the file path
-        # Check if the file is a .doc, .txt, .csv, or .xlsx file
-        if file.endswith(('.doc', '.txt', '.csv', '.xlsx',)):
-            # Create a subdirectory based on the file extension
-            subdirectory = os.path.join(directory, file.split('.')[-1])
-            os.makedirs(subdirectory, exist_ok=True)
-            # Move the file to the subdirectory
-            new_file_path = os.path.join(subdirectory, file)
-            shutil.move(file_path, new_file_path)
-            #breakpoint()  # Pause to inspect the file moving operation
-    print("File organization completed.") """
 
-""" def file_organizer(directory):
-    # Get the list of all files in the directory
-    files = os.listdir(directory)
-    print("Scanning for files in directory:", directory)
-    # Loop through each file in the directory
-    files_scanned = 0
-    files_organized = 0
-    for i, file in enumerate(files):
-        # Get the full file path
-        file_path = os.path.join(directory, file)
-        # Check if the file is a .doc, .txt, .csv, or .xlsx file
-        if file.endswith(('.doc', '.txt', '.csv', '.xlsx',)):
-            # Create a subdirectory based on the file extension
-            subdirectory = os.path.join(directory, file.split('.')[-1])
-            os.makedirs(subdirectory, exist_ok=True)
-            # Move the file to the subdirectory
-            new_file_path = os.path.join(subdirectory, file)
-            shutil.move(file_path, new_file_path)
-            files_organized += 1
-        files_scanned += 1
-        # Update progress every 10 files
-        if i % 5 == 0:
-            print(f"Processed {i+1} files out of {len(files)}")
-    print("Scanning and organizing files completed.")
-    print(f"Total files scanned: {files_scanned}")
-    print(f"Total files organized: {files_organized}")
-#This code will print out a progress message every 10 files processed and then print the final results of the scanning 
-#and organizing process, including the total number of files scanned and the total number of files organized. """
-
-
+def search_files(directory_path, text_pattern):
+    try:
+        # Define the file extensions to search for
+        file_extensions = (".txt", ".doc", ".csv", ".xlsx", ".docx",)        
+        # Create an empty list to store the matching file paths
+        files_found = []       
+        # Compile the regular expression pattern
+        pattern = re.compile(text_pattern, re.IGNORECASE)
+        # Use os.walk() to recursively search for files in the directory
+        for subdir, dirs, files in os.walk(directory_path):
+            for file in files:
+                # Check if the file extension matches any of the desired extensions
+                file_ext = os.path.splitext(file)[-1].lower()
+                if file_ext in file_extensions:
+                    # If a match is found, add the file path to the list
+                    file_path = os.path.join(subdir, file)
+                    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                        for line in f:
+                            if pattern.search(line):
+                                files_found.append(file_path)
+                                break
+        # If one or more files are found, return their paths and count
+        if files_found:
+            result = "Files found:\n"
+            for file in files_found:
+                result += f"File name: {os.path.basename(file)}\nFile path: {file}\n"
+            result += f"Total number of search files: {len(files_found)}"
+            print(result)
+            return result
+        else:
+            print("No files found.")
+            return "No files found."  
+    except FileNotFoundError as e:
+        error_msg = f"Error: {e.strerror}: '{directory_path}'"
+        print(error_msg)
+        raise Exception(error_msg)
+    except Exception as e:
+        error_msg = f"Error: {e}"
+        print(error_msg)
+        raise Exception(error_msg)
+    
 class CancelExecution(Exception):
     pass
 def signal_handler(signal, frame):
@@ -223,32 +169,29 @@ def file_organizer(directory):
     # Loop through each file in the directory
     files_scanned = 0
     files_organized = 0
-    with tqdm(total=len(files), unit="file") as progress_bar:
-        signal.signal(signal.SIGINT, signal_handler)
-        for file in files:
-            # Get the full file path
-            file_path = os.path.join(directory, file)
-            # Add print statement scanning with file name.
-            print(f"Scanning file: {file}")
-            # Check if the file is a .doc, .txt, .csv, or .xlsx file
-            if file.endswith(('.txt', '.csv', '.xlsx', '.doc',)):
-                # Create a subdirectory based on the file extension
-                subdirectory = os.path.join(directory, file.split('.')[-1])
-                os.makedirs(subdirectory, exist_ok=True)
-                # Move the file to the subdirectory
-                new_file_path = os.path.join(subdirectory, file)
-                shutil.move(file_path, new_file_path)
-                files_organized += 1
-            files_scanned += 1
-            progress_bar.update(1)
-            # Check for user input to cancel process
-            if input("Press 'q' to cancel file organizing process: ") == 'q':
-                if input("Do you want to continue with the process? (y/n): ") == 'n':
-                    print("File organizing process cancelled.")
-                    return
+    for i, file in enumerate(files):
+        # Get the full file path
+        file_path = os.path.join(directory, file)
+        print(f"Scanning file: {file}")
+        # Check if the file is a .doc, .txt, .csv, or .xlsx file
+        if file.endswith(('.doc', '.txt', '.csv', '.xlsx',)):
+            # Create a subdirectory based on the file extension
+            subdirectory = os.path.join(directory, file.split('.')[-1])
+            os.makedirs(subdirectory, exist_ok=True)
+            # Move the file to the subdirectory
+            new_file_path = os.path.join(subdirectory, file)
+            shutil.move(file_path, new_file_path)
+            files_organized += 1
+        files_scanned += 1
+        # Update progress every 5 files
+        if i % 5 == 0:      
+            print(f"Processed {i+1} files out of {len(files)}")
     print("Scanning and organizing files completed.")
     print(f"Total files scanned: {files_scanned}")
     print(f"Total files organized: {files_organized}")
+    #This code will print out a progress message every 5 files processed and then print the final results of the scanning 
+    #and organizing process, including the total number of files scanned and the total number of files organized.
+
 
 def main():
     # call function to organize the root directory by placing 3 types of files into respective sub-directories
